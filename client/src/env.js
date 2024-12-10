90251cc731ca6a22bbf73346a2bc0a3696a7b53a
heather bickish <hbickish@gmail.com>
2024-12-10 15:20:43 -0700

    created tower event model and registered in dbcontext

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-3s02e438pmb5lzcl.us.auth0.com'
+export const clientId = 'iQGnIrm4RXpgggICyQUVkMKWfVRRO1kN'
+export const audience = 'https://auth.gogetterfoundation.com'
---
+++ server/src/models/TowerEvent.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+
+export const TowerEventSchema = new Schema({
+  creatorId: { type: Schema.ObjectId, required: true },
+  name: { type: String, minLength: 3, maxLength: 50, required: true },
+  description: { type: String, minLength: 15, maxLength: 1000 },
+  coverImg: { type: String, maxLength: 1000, required: true },
+  location: { type: String, minLength: 1, maxLength: 500, required: true },
+  capacity: { type: Number, min: 1, max: 5000, required: true },
+  startDate: { type: Date, required: true },
+  isCanceled: { type: Boolean, default: false },
+  type: { type: String, enum: ['concert', 'convention', 'sport', 'digital'] }
+},
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+  }
+)
