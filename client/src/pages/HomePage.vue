54824e9d050368e8c107c10cc366841c8ffba771
heather bickish <hbickish@gmail.com>
2024-12-10 18:30:52 -0700

    getting events, added hero img

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { AppState } from "@/AppState";
 import { towerEventsService } from "@/services/TowerEventsService";
 import { logger } from "@/utils/Logger";
 import Pop from "@/utils/Pop";
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
+
+const towerEvents = computed(() => AppState.towerEvents)
 
 onMounted(() => {
   getEvents()
@@ -20,7 +23,21 @@ async function getEvents() {
 </script>
 
 <template>
-  <h1>home page</h1>
+  <section class="container">
+    <div class="row hero">
+      <div class="col-12">
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.hero {
+  background-image: url(https://images.unsplash.com/photo-1467810563316-b5476525c0f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBhcnR5fGVufDB8fDB8fHwy);
+  height: 500px;
+  width: auto;
+  background-size: cover;
+  background-position: center;
+
+}
+</style>
--- client/src/services/TowerEventsService.js
+++ client/src/services/TowerEventsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { TowerEvent } from "@/models/TowerEvent.js"
+import { AppState } from "@/AppState.js"
 
 class TowerEventsService {
   async getEvents() {
     const response = await api.get('api/events')
-    logger.log('Got events', response.data)
+    const events = response.data.map(eventPojo => new TowerEvent(eventPojo))
+    AppState.towerEvents = events
+    logger.log(events)
   }
 
 }
