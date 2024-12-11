66c866d6aea8ccb188e2d8e509089dbca85c7ea7
heather bickish <hbickish@gmail.com>
2024-12-11 16:35:37 -0700

    deleting ticket

--- server/src/controllers/TicketsController.js
+++ server/src/controllers/TicketsController.js
@@ -8,6 +8,7 @@ export class TicketsController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createTicket)
+      .delete('/:ticketId', this.deleteTicket)
   }
 
 
@@ -22,7 +23,16 @@ export class TicketsController extends BaseController {
     }
   }
 
-
+  async deleteTicket(request, response, next) {
+    try {
+      const ticketId = request.params.ticketId
+      const userId = request.userInfo.id
+      const message = await ticketsService.deleteTicket(ticketId, userId)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
 
 
 }
--- server/src/services/TicketsService.js
+++ server/src/services/TicketsService.js
@@ -1,6 +1,14 @@
 import { dbContext } from "../db/DbContext"
+import { Forbidden } from "../utils/Errors"
 
 class TicketsService {
+  async deleteTicket(ticketId, userId) {
+    const ticketToDelete = await dbContext.Tickets.findById(ticketId)
+    if (ticketToDelete == null) { throw new Error(`Invalid ticket id ${ticketId}`) }
+    if (ticketToDelete.accountId != userId) { throw new Forbidden("Get outta here! You can't delete someone else's ticket brah") }
+    await ticketToDelete.deleteOne()
+    return 'Ticket gone'
+  }
   async getEventTickets(eventId) {
     const tickets = await dbContext.Tickets.find({ eventId: eventId }).populate('profile', 'name picture')
     return tickets
