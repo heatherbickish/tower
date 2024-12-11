72ef9e9b726dedf2b530e26d53966899e346b2bb
heather bickish <hbickish@gmail.com>
2024-12-10 19:44:16 -0700

    displaying events on home page

---
+++ client/src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { TowerEvent } from "@/models/TowerEvent";
+
+defineProps({
+  towerEvent: { type: TowerEvent, required: true }
+})
+</script>
+
+
+<template>
+  <div class="mx-3">
+    <img :src="towerEvent.coverImg" alt="" class="cover-img">
+  </div>
+  <p>{{ towerEvent.name }}</p>
+  <small>
+    {{ towerEvent.location }}
+  </small>
+</template>
+
+
+<style lang="scss" scoped>
+.cover-img {
+  min-height: 30dvh;
+  background-size: cover;
+  background-position: center;
+
+
+}
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from "@/AppState";
+import EventCard from "@/components/EventCard.vue";
 import { towerEventsService } from "@/services/TowerEventsService";
 import { logger } from "@/utils/Logger";
 import Pop from "@/utils/Pop";
@@ -25,7 +26,68 @@ async function getEvents() {
 <template>
   <section class="container">
     <div class="row hero">
+      <div class="col-md-6">
+        <div class="d-flex justify-content-end flex-column flex-end text-light p-4 ms-4 lh-2">
+          <h3>Event management for people, by people</h3>
+          <p>Whatever your interests, from hiking and reading to networking and skill sharing, there are thousands of
+            people who share it on Tower Events are happening every day-log in to join the fun.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
       <div class="col-12">
+        <div class="m-5 px-3">
+          <h4>How Tower works</h4>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-around">
+      <div class="col-md-4 bg-light">
+        <div class="p-3">
+          <i class="mdi mdi-magnify text-success fs-4 me-3"></i>
+          <h5>Discover events you're interested in</h5>
+          <small>Browse through community hosted events for all the things you love</small>
+        </div>
+      </div>
+      <div class="col-md-4 bg-light">
+        <div class="p-3">
+          <i class="mdi mdi-plus text-success fs-4 me-3"></i>
+          <h5>Discover events you're interested in</h5>
+          <small>Create your own Tower event, and draw from a community of millions</small>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="m-5 px-3">
+          <h4>Explore top categories</h4>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-1 bg-light">
+            <div class="text-center py-2">
+              <i class="mdi mdi-all-inclusive text-success"></i>
+              <h6>All</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="m-5 px-3">
+          <h4>Upcoming events</h4>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div v-for="towerEvent in towerEvents" :key="towerEvent.id" class="col-4">
+          <EventCard :towerEvent="towerEvent" />
+        </div>
       </div>
     </div>
   </section>
